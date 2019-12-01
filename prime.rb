61ecd18a592d9913ee8953dc7f606fba106a918e
Hoko Le <hokole527@gmail.com>
2019-12-01 22:36:35 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+def prime?(n)
+
+    if n <= 1
+        return false
+    elsif n == 2
+        return true
+    elsif (n/2).each {|i| i === i.floor}
+      return false
+    else
+      return true
+    end
+    
+
+  
+end 
