e273075d47884f8d4dfeb83106ca27dc86cfb523
Hoko Le <hokole527@gmail.com>
2019-12-01 22:44:08 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,9 +4,9 @@ return false !n.integer?
 
 return false if n <= 1
 
-    elsif n == 2
-        return true
-    elsif (2..n-1).each {|i| if n % i == 0}
+return true if n == 2
+
+(2..n-1).each {|i| if n % i == 0}
       return false
     else
       return true
